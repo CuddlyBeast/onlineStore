df024a8725d9e4aa73a38d455e7cc1c33e2eea78
CuddlyBeast <147174642+CuddlyBeast@users.noreply.github.com>
2024-03-21 23:02:09 +0000

    bulk of order history page is comeplete

--- public/orders.js
+++ public/orders.js
@@ -16,9 +16,131 @@ if (close) {
 }
 
 document.addEventListener('DOMContentLoaded', async function() {
+    const orderList = document.querySelector('.order-list');
+    const token = localStorage.getItem('token');
+
+    if (token) {
+        try {
+            const response = await fetch('http://localhost:3000/cuddy/order/history', {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${token}`
+                }
+            });
+
+            if (response.ok) {
+                let orders = await response.json();
+
+                orders.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
+
+                for (const order of orders) {
+                    // Create a div element for each order
+                    const orderItem = document.createElement('div');
+                    orderItem.classList.add('order-item');
+
+                    orderItem.innerHTML = `
+                        <h3>Order #${order.id}</h3> 
+                        <button class="delete-order" data-order-id="${order.id}"><i class='bx bx-trash'></i></button>
+                        <span>Order Status: ${order.orderStatus}</span>
+                        <p>Time of Purchase: ${order.createdAt.slice(11, 19)} | ${order.createdAt.slice(0, 10)}</p>
+                        <p>Delivery/Billing Address: ${order.address}, ${order.city}, ${order.postcode},  ${order.country}</p>
+                        <p>Payment: ${order.paymentInformation}</p>
+                        <p>Discount: $${order.discount}</p>
+                        <p>Delivery Type: ${order.deliveryType}</p>
+                        <p>Total: <strong>$${order.totalAmount}</strong></p>
+                        <div class="order-details-${order.id}"></div>
+                    `;
+                    const orderDetailsResponse = await fetch(`http://localhost:3000/cuddy/order/${order.id}/details`, {
+                        method: 'GET',
+                        headers: {
+                            'Content-Type': 'application/json',
+                            'Authorization': `Bearer ${token}`
+                        }
+                    });
+                    if (orderDetailsResponse.ok) {
+                        const orderDetails = await orderDetailsResponse.json();
+                        const orderDetailsContainer = orderItem.querySelector(`.order-details-${order.id}`);
+
+                        for (const detail of orderDetails) {
+                            const productResponse = await fetch(`http://localhost:3000/cuddy/products/${detail.productId}`, {
+                                method: 'GET',
+                                headers: {
+                                    'Content-Type': 'application/json',
+                                }
+                            });
+
+                            if (productResponse.ok) {
+                                const productData = await productResponse.json();
+
+                                orderDetailsContainer.innerHTML += `
+                                    <div class="product-details">
+                                        <img src="${productData.image}" alt="${productData.name}">
+                                        <p>${productData.name}</p>
+                                        <p>$${detail.price}</p>
+                                        <p>Quantity: ${detail.quantity}</p>
+                                        <p>Size: ${detail.size}</p>
+                                    </div>
+                                `;
+                            } else {
+                                console.error('Failed to fetch product details');
+                            }
+                        }
+                    } else {
+                        console.error('Failed to fetch order details');
+                    }
+
+                    orderList.appendChild(orderItem);
+                }
+
+                document.querySelectorAll('.delete-order').forEach(button => {
+                    button.addEventListener('click', async () => {
+                        const orderId = parseInt(button.dataset.orderId);
+                        try {
+                            const response = await fetch(`http://localhost:3000/cuddy/order/${orderId}`, {
+                                method: 'DELETE',
+                                headers: {
+                                    'Authorization': `Bearer ${token}`,
+                                }
+                            });
+                            if (!response.ok) {
+                                throw new Error('Failed to delete order');
+                            }
+                            location.reload();
+                        } catch (error) {
+                            console.error('Error deleting order:', error);
+                        }
+                    });
+            
+                   // Get the order creation time
+                   const orderId = parseInt(button.dataset.orderId);
+                   const order = orders.find(order => order.id === orderId);
+                   const orderCreationTime = new Date(order.createdAt).getTime();
+            
+            
+                   // Calculate the time elapsed since the order was made
+                   const currentTime = new Date().getTime();
+                   const timeElapsed = currentTime - orderCreationTime;
+            
+                   // Set the timeout to hide the delete button after five minutes since the order was made
+                   const timeLimit = 300000;
+                   const timeRemaining = Math.max(timeLimit - timeElapsed, 0);
+                   setTimeout(() => {
+                       button.style.display = 'none';
+                   }, timeRemaining);
+               });
+            } else {
+                console.error('Failed to fetch order history');
+            }
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    } else {
+        console.error('Token not found');
+    }
+
     const profileIcon = document.querySelector('.profile-dropdown');
     const profileDropdownContent = document.querySelector('.profile-dropdown-content');
-    const token = localStorage.getItem('token');
 
     const showProfileDropdownContent = () => {
         profileDropdownContent.style.display = 'block'; 
